0d282b1a52c4f81e351474cf11bda3f0d79c4625
alant79 <a.alexeyev@mail.ru>
2021-07-12 14:56:47 +0300

    cookie1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(expressSession({
   secret: 'keyboard cat', resave: true,
-  saveUninitialized: true, cookie: { maxAge: 60000, secure: false }
+  saveUninitialized: true, cookie: { maxAge: 60000, secure: false, httpOnly: false }
 }))
 
 app.post('/auth', function (req, res) {
