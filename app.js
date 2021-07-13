dc3e65f236a51f1a246c426f8facaa88e34a11cb
alant79 <a.alexeyev@mail.ru>
2021-07-13 13:32:18 +0300

    setData

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.post('/setData', function (req, res) {
     const {user} =  req.body 
     console.log(`${user.toLowerCase()}.json`)
     console.log(req.body)
-    fs.writeFileSync(`${user.toLowerCase()}.json`,JSON.stringify(req.body, null, 4))
+    fs.writeFileSync(__dirname + `/${user.toLowerCase()}.json`,JSON.stringify(req.body, null, 4))
     res.send('ok');
 
   } catch (err) {
