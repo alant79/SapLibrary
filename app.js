e13cc85a9b1330a6ebfbede53cd25b168972a513
alant79 <a.alexeyev@mail.ru>
2021-08-30 11:28:33 +0300

    setFileBinary

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.post('/setFileBinary', function (req, res) {
   const fileid = req.body.FILEID || req.body.fileid
   const username = req.body.USERNAME || req.body.username
   const filedata = req.body.FILEDATA || req.body.filedata
-  const fileName = req.body.FILENAME || req.body.fileName
+  const fileName = req.body.FILENAME || req.body.filename
   var pathFile = path.join(__dirname, 'uploads', fileName)
   fs.writeFile(pathFile, filedata, function (err) {
     collection.updateOne({ fileid, username }, {
